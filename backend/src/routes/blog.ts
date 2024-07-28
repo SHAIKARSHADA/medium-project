5aced2a79480ef6cb2f72e9023e6a3140b10c278
Shaik <shaikarshad1792004@gmail.com>
2024-07-28 23:39:40 +0530

    Some more Debugging

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,19 +1,19 @@
 import { Hono } from "hono";
-import { verify, sign } from 'hono/jwt'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { verify, sign } from "hono/jwt";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 // type Variables = string;
 
 export const blogRouter = new Hono<{
   Bindings: {
-    DATABASE_URL: string,
-    SECRET_URL: string,
-  },
+    DATABASE_URL: string;
+    SECRET_URL: string;
+  };
   Variables: {
-    userId: string,
-  }
-}>()
+    userId: string;
+  };
+}>();
 
 blogRouter.use('/*', async (c, next) => {
   const authHeader = c.req.header("Authorization") || "";
@@ -23,18 +23,24 @@ blogRouter.use('/*', async (c, next) => {
       message: "unauthorised",
     })
   }
+
+  console.log("I am Inside");
   // @ts-ignore
-    c.set("userId", user.id)
-    await next();
+  c.set("userId", user.id)
+  await next();
+  console.log("I am outside");
 })
 
-blogRouter.post('/', async (c) => {
+// This route is working properly
+
+blogRouter.post("/", async (c) => {
   const body = await c.req.json();
   const userId = c.get("userId");
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-  
+
+
   console.log(body);
   const response = await prisma.post.create({
     data: {
@@ -42,66 +48,65 @@ blogRouter.post('/', async (c) => {
       content: body.content,
       // @ts-ignore
       authorId: userId,
-    }
-  })
+    },
+  });
 
-  return c.json(response.id)
-})
+  return c.json(response.id);
+});
 
+// This route is working properly
 
 blogRouter.put('/', async (c) => {
-  const body = await c.req.json();
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  const blog = await prisma.post.create({
+  const body = await c.req.json();
+  const userId = c.get("userId");
+
+  const blog = await prisma.post.update({
+    where: {
+      id: body.id,
+      //@ts-ignore
+      authorId: userId,
+    },
     data: {
       title: body.title,
       content: body.content,
-      authorId: body.userId,
     }
   })
 
-  return c.json({
-    id: blog.id,
-  })
+  return c.json(blog);
 })
 
-blogRouter.get('/:id', async (c) => {
-  const body = await c.req.json();
+
+// In here the issue was it was needed an integer and I was given an string to it so, that was the error
+blogRouter.get("/:id", async (c) => {
+  const idParam = c.req.param("id"); // 
+  const id = parseInt(idParam, 10);
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
   try {
-
-    const blog = await prisma.post.findFirst({
+    const blog = await prisma.post.findUnique({
       where: {
-        id: body.id
+        // @ts-ignore
+        id: id,
       },
-      select: {
-        title: body.title,
-        content: body.content,
-        authorId: body.userId,
-      }
-    })
-  } catch(e) {
-    c.status(411);
+    });
+    return c.json(blog);
+  } catch (e) {
+    c.status(500);
     c.json({
       message: "error while fetching data",
-    })
+    });
   }
 });
 
-blogRouter.get('/bulk',async (c) => {
+blogRouter.get('/bulk', async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-   
-  const blog = await prisma.post.findMany();
 
-  return c.json({
-    blog
-  })
-
-})
+  return c.text("hello");
+});

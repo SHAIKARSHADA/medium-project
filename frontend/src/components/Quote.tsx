aec63298095a8a1ad59d3ff254b2abd066b1466a
Shaik <shaikarshad1792004@gmail.com>
2024-08-01 04:02:01 +0530

    fixed: some tailwind classes fixed

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -4,12 +4,13 @@ export const Quote = () => {
       <div className="flex justify-center ">
         <div className="max-w-lg">
           <div className="text-2xl font-extrabold ">
-          "An exceptional blogging site. It offered a seamless experience and great features."
+            "An exceptional blogging site. It offered a seamless experience and
+            great features."
           </div>
           <div className="max-w-md mt-2 text-xl font-semibold ">
             Jules winnfield
           </div>
-          <div className="max-w-md text-sm font-light font-semibold text-slate-400 ">
+          <div className="max-w-md text-sm font-light text-slate-400 ">
             CEO | Ace corp
           </div>
         </div>
